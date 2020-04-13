a4564df9d3ea676beedc68fde62233bdddc5b9b3
Eronan <animeftw@live.co.uk>
2020-04-13 19:50:14 +1000

    Shimzu Gyogun bug

--- script/c902381001.lua
+++ script/c902381001.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--rune
 	c:EnableReviveLimit()
-	aux.AddRuneProcedure(c,nil,1,1,nil,1,1)
+	Rune.AddProcedure(c,nil,1,1,nil,1,1)
 	--level change
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
