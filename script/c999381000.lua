4ec93106aaa44e36d5eb4e0919b051627970b587
Eronan <animeftw@live.co.uk>
2020-04-25 15:03:43 +1000

    Fix Greenwood

--- script/c999381000.lua
+++ script/c999381000.lua
@@ -8,6 +8,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
@@ -16,7 +17,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.rvfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsPublic()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.rvfilter,tp,LOCATION_HAND,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+    local g=Duel.SelectMatchingCard(tp,s.rvfilter,tp,LOCATION_HAND,0,1,1,nil)
+    Duel.ConfirmCards(1-tp,g)
+    Duel.ShuffleHand(tp)
 end
 function s.thfilter(c,mc)
 	return c:IsType(TYPE_RUNE) and c:IsAbleToHand()
--- script/c999381001.lua
+++ script/c999381001.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCost(s.tfcost)
 	e1:SetTarget(s.tftg)
 	e1:SetOperation(s.tfop)
 	c:RegisterEffect(e1)
@@ -15,7 +16,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.rvfilter(c)
-	return c:IsType(TYPE_RUNE)
+	return c:IsType(TYPE_RUNE) and not c:IsPublic()
+end
+function s.tfcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.rvfilter,tp,LOCATION_HAND,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+    local g=Duel.SelectMatchingCard(tp,s.rvfilter,tp,LOCATION_HAND,0,1,1,nil)
+    Duel.ConfirmCards(1-tp,g)
+    Duel.ShuffleHand(tp)
 end
 function s.tffilter(c,rc)
 	return (c:GetType()&TYPE_CONTINUOUS+TYPE_TRAP)==TYPE_CONTINUOUS+TYPE_TRAP and not c:IsForbidden()
