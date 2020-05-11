78c702f14178a4f8683b5a6904659033a7f377b1
Eronan <animeftw@live.co.uk>
2020-05-11 21:26:28 +1000

    Fix Duplicity Summoning Circle Bug

--- script/c900013928.lua
+++ script/c900013928.lua
@@ -12,12 +12,12 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0xfe8}
-function s.runfilter(c,e)
+function s.plyrfilter(c,e)
 	return c:IsSetCard(0xfe8) and c:IsRuneSummonable(e:GetHandler())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local b1=Duel.IsExistingMatchingCard(s.runfilter,tp,LOCATION_HAND,0,1,nil,e)
+	local b1=Duel.IsExistingMatchingCard(s.plyrfilter,tp,LOCATION_HAND,0,1,nil,e)
 	local b2=true
 	local op=0
 	if b1 and b2 then
@@ -35,32 +35,35 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.plyroperation(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsExistingMatchingCard(s.runfilter,tp,LOCATION_HAND,0,1,nil,e) then return end
+	if not Duel.IsExistingMatchingCard(s.plyrfilter,tp,LOCATION_HAND,0,1,nil,e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.runfilter,tp,LOCATION_HAND,0,1,1,nil,e)
+	local g=Duel.SelectMatchingCard(tp,s.plyrfilter,tp,LOCATION_HAND,0,1,1,nil,e)
 	if g:GetCount()>0 then
 		local sc=g:GetFirst()
 		Duel.RuneSummon(tp,sc,e:GetHandler())
 	end
 end
-function s.oppfilter(c,e,tp,exg)
-	local runeGroup=Rune.DefaultGroup(c,tp)
-	return c:IsRuneSummonable(e:GetHandler(),runeGroup+exg)
-end
 function s.exfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsFaceup()
+end
+function s.oppfilter(c,e,tp,exg)
+	local dfg=Rune.DefaultGroup(c,1-tp)
+	if dfg then
+		return c:IsRuneSummonable(e:GetHandler(),dfg+exg)
+	else return false end
 end
 function s.oppoperation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local exg=Duel.GetMatchingGroup(s.exfilter,tp,LOCATION_ONFIELD,0,nil)
 	if Duel.IsExistingMatchingCard(s.oppfilter,tp,0,0x3ff~LOCATION_MZONE,1,nil,e,tp,exg)
-			and Duel.SelectYesNo(1-tp,aux.Stringid(30241314,0)) then
+			and Duel.SelectYesNo(1-tp,aux.Stringid(id,3)) then
 		--Summon
 		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(1-tp,s.oppfilter,tp,0,0x3ff~LOCATION_MZONE,1,1,nil,e,tp,exg)
 		if g:GetCount()>0 then
 			local sc=g:GetFirst()
-			Duel.RuneSummon(1-tp,sc,e:GetHandler(),Rune.DefaultGroup(sc,tp)+exg)
+			local dfg=Rune.DefaultGroup(sc,1-tp)
+			Duel.RuneSummon(1-tp,sc,e:GetHandler(),exg+dfg)
 		end
 	else
 		--Send to the Grave
@@ -68,17 +71,6 @@ function s.oppoperation(e,tp,eg,ep,ev,re,r,rp)
 		local tc=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
 		if tc then
 			Duel.SendtoGrave(tc,REASON_EFFECT)
-		end
-	end
-end
-function s.RuneExtra(chk,summon_type,e,...)
-	local c=e:GetHandler()
-	if chk==0 then
-		local tp,sc=...
-		if not summon_type==SUMMON_TYPE_RUNE then
-			return Group.CreateGroup()
-		else
-			return e:GetLabelObject()
 		end
 	end
 end
