7fc237711b590d354d7eb7ab68e79bd1a1be2e70
Eronan <animeftw@live.co.uk>
2021-02-01 18:53:37 +1100

    Fix Hydranic Lord

--- script/c962318972.lua
+++ script/c962318972.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Rune Summon
 	c:EnableReviveLimit()
-	Rune.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_WATER),1,1,aux.FilterBoolFunctionEx(Card.IsRuneCode,22702055),1,99)
+	Rune.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_WATER),1,99,aux.FilterBoolFunctionEx(Card.IsRuneCode,22702055),1,1)
 	--atkup
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
@@ -14,39 +14,46 @@ function s.initial_effect(c)
 	e2:SetTarget(s.cdtg)
 	e2:SetValue(22702055)
 	c:RegisterEffect(e2)
-	--
+	--register effect
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
-	e3:SetCountLimit(s.desval)
-	e3:SetValue(s.valcon)
-	e3:SetLabel(0)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(s.regcon)
+	e3:SetOperation(s.regop)
 	c:RegisterEffect(e3)
-	--
+	--material count check
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_MATERIAL_CHECK)
+	e4:SetValue(s.valcheck)
 	e4:SetLabelObject(e3)
-	e4:SetOperation(s.regop)
 	c:RegisterEffect(e4)
 end
 s.listed_names={22702055}
 function s.cdtg(e,c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c~=e:GetHandler()
 end
-function s.desval(e)
-	return e:GetLabel()
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	local ct=g:FilterCount(Card.IsAttribute,nil,ATTRIBUTE_WATER)
+	e:GetLabelObject():SetLabel(ct)
 end
-function s.valcon(e,re,r,rp)
-	return bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_RUNE) and c:GetMaterial():FilterCount(Card.IsAttribute,nil,ATTRIBUTE_WATER)>0
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:GetSummonType()==SUMMON_TYPE_RUNE then
-		local ct=c:GetMaterialCount()-1
-		e:GetLabelObject():SetLabel(ct)
-	end
+	local ct=e:GetLabel()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetCountLimit(ct)
+	e1:SetValue(s.valcon)
+	c:RegisterEffect(e1)
+end
+function s.valcon(e,re,r,rp)
+	return bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0
 end
