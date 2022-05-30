585b35608bf50ff1927414a5b671e12b871e7cf7
Eronan <animeftw@live.co.uk>
2022-05-30 13:48:13 +1000

    Update c998432104.lua

--- script/c998432104.lua
+++ script/c998432104.lua
@@ -60,7 +60,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SpecialSummonComplete()
 end
 function s.immcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_RUNE)
+	return Duel.IsExistingMatchingCard(Card.IsType,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,TYPE_RUNE)
 end
 function s.etarget(e,c)
 	return c==e:GetHandler() or e:GetHandler():GetLinkedGroup():IsContains(c)
