a56fa85271fa6beaa20d40526b01308728c44ae8
Eronan <animeftw@live.co.uk>
2023-05-04 17:20:58 +1000

    Update Deprecated FilterFaceupFunction

--- script/c918203026.lua
+++ script/c918203026.lua
@@ -40,9 +40,9 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.accon(e)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x16e),0,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x16e),0,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 		and (Duel.IsEnvironment(7142724)
-		or Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,7142724),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil))
+		or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,7142724),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil))
 end
 function s.aclim(e,re,tp)
 	local rc=re:GetHandler()
