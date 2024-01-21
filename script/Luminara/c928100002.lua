0c3b2b7acc4cad16f718f2bae82c7fad9341670a
Ere <animeftw@live.co.uk>
2024-01-21 13:17:47 +1100

    Modify Luminaras Mirror Chanter

--- script/Luminara/c928100002.lua
+++ script/Luminara/c928100002.lua
@@ -5,13 +5,6 @@ function s.initial_effect(c)
     Pendulum.AddProcedure(c,false)
     Link.AddProcedure(c,nil,2,4,s.lcheck)
     c:EnableReviveLimit()
-    --Treat as Link Spell
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_ADD_TYPE)
-    e1:SetRange(LOCATION_PZONE)
-    e1:SetValue(TYPE_LINK)
-    c:RegisterEffect(e1)
 end
 function s.lcheck(g,lc,sumtype,tp)
 	return g:IsExists(aux.NOT(Card.IsType),1,nil,TYPE_EFFECT,lc,sumtype,tp)
