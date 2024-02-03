23a65e83426a9cca1a38954bced058845b4a8190
Ere <animeftw@live.co.uk>
2024-02-03 14:19:57 +1100

    Add Insigniate Etherunetation

---
+++ script/Insigniate/c900001037.lua
@@ -0,0 +1,101 @@
+--Insigniate Trap
+if not Rune then Duel.LoadScript("proc_rune.lua") end
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+    --Extra Material
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCode(EFFECT_EXTRA_MATERIAL)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,0)
+	e3:SetOperation(s.extracon)
+	e3:SetValue(s.extraval)
+	e3:SetTarget(s.sendloc)
+	c:RegisterEffect(e3)
+	if s.flagmap==nil then
+		s.flagmap={}
+	end
+	if s.flagmap[c]==nil then
+		s.flagmap[c] = {}
+	end
+end
+--rune 
+function s.filter1(c,e,tp)
+	return c:IsFaceup() and c:IsType(TYPE_RUNE)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,e,tp,c)
+end
+function s.filter2(c,e,tp,mc)
+	return c:ListsCode(mc:GetCode()) and c:IsType(TYPE_RUNE) and c:IsRuneCustomCheck(Group.FromCards(e:GetHandler(),mc),tp)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RUNE,tp,false,true) and c:GetLevel()>mc:GetLevel()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter1(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<0 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc)
+	local sc=g:GetFirst()
+	if sc then
+		local mg=Group.FromCards(tc,e:GetHandler())
+		Duel.BreakEffect()
+		if sc:IsRuneCustomCheck(mg,tp) then
+			sc:SetMaterial(mg)
+			Duel.SendtoGrave(mg,REASON_EFFECT+REASON_MATERIAL+REASON_RUNE)
+			Duel.SpecialSummon(sc,SUMMON_TYPE_RUNE,tp,tp,false,true,POS_FACEUP)
+		end
+		sc:CompleteProcedure()
+		mg:DeleteGroup()
+	end
+end
+--Extra Rune Material
+function s.extracon(c,e,tp,sg,mg,lc,og,chk)
+	return not sg or sg:FilterCount(s.flagcheck,nil)<2
+end
+function s.flagcheck(c)
+	return c:GetFlagEffect(id)>0
+end
+function s.extraval(chk,summon_type,e,...)
+	local c=e:GetHandler()
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_RUNE or Duel.GetFlagEffect(tp,id)>0 then
+			return Group.CreateGroup()
+		else
+			table.insert(s.flagmap[c],c:RegisterFlagEffect(id,0,0,1))
+			return Group.FromCards(c)
+		end
+	elseif chk==1 then
+		local sg,sc,tp=...
+		if summon_type&SUMMON_TYPE_RUNE == SUMMON_TYPE_RUNE and #sg>0 and Duel.GetFlagEffect(tp,id)==0 then
+			Duel.Hint(HINT_CARD,tp,id)
+			Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+		end
+	elseif chk==2 then
+		for _,eff in ipairs(s.flagmap[c]) do
+			eff:Reset()
+		end
+		s.flagmap[c]={}
+	end
+end
+function s.sendloc(c,e,tp,sg,ug,rc,chk)
+	return LOCATION_REMOVED
+end
