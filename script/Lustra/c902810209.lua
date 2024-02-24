94abddf34254343828040843199ad089f0cbe4a2
Ere <animeftw@live.co.uk>
2024-02-24 23:49:58 +1100

    Fixes

--- script/Lustra/c902810209.lua
+++ script/Lustra/c902810209.lua
@@ -104,7 +104,7 @@ end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
     local fromfield=e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
     if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_DECK,0,1,nil)
-        and (not fromfield or Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil)) end
+        and (not fromfield or Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)) end
     Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
     if fromfield then
         e:GetLabel(1)
